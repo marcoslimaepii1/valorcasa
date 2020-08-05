0e7e78dd6a1139558871b3e84b3db4d14df12356
Marcos Lima <marcoslima@lccv.ufal.br>
2020-08-05 13:55:26 -0300

    update

--- nao_entre_em_panico.py
+++ nao_entre_em_panico.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def nao_entre_em_panico():
-    if request.headers.get('Authorization') == '42':
+    if request.args.get('key') == '42':
         return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
-    return jsonify({"message": "Não entre em pânico!"})
+    return request.key.get('key')
+    # return jsonify({"message": "Não entre em pânico!"})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
